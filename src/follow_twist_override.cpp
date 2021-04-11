d1394efdcdf588ede2ec4115ce0c85f83db640a7
Brian Neumeyer <bpneumey@oakland.edu>
2021-04-11 17:24:04 -0400

    Node Launches
    Node launches, does not yet override parameter

--- src/follow_twist_override.cpp
+++ src/follow_twist_override.cpp
@@ -15,13 +15,22 @@ void speedTimerCallback(const ros::TimerEvent& event)
   //ack_vel.angular.z = psi_doECE_5532_Final_Projectt;
 
   pub_vel.publish(vel);
+
+  ROS_INFO("Published velocity");
 }
 
 //main function
 int main(int argc, char** argv){
 //init section
-  ros::init(argc,argv,"gps_sim_proj");
+  ros::init(argc,argv,"follow_twist_override");
   ros::NodeHandle nh;
 
+  ROS_INFO("INTO MAIN");
+
+  ros::Timer timer = nh.createTimer(ros::Duration(1.0), speedTimerCallback);
+
   pub_vel = nh.advertise<geometry_msgs::Twist>("/override/cmd_vel", 1); //x velocity with float64
+  //ros::Subscriber sub_twist = nh.subscribe("/audibot/throttle_cmd", 1, recvThr);
+
+  ros::spin();
 }
