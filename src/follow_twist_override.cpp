14961e32d8ad86cd430eb6bfe805727d1516ae36
Brian Neumeyer <bpneumey@oakland.edu>
2021-04-14 22:59:42 -0400

    Code Cleanup
    Removed unnecessary lines, commented code

--- src/follow_twist_override.cpp
+++ src/follow_twist_override.cpp
@@ -16,26 +16,19 @@ double a1_a2_separation = 0;
 double sep_target = 18;
 //current turn command phi
 double cmd_turn; 
-//Previous following vehicle velocity
-double prev_vel;
 //vehicle max & min velocities
 double max_vel = 25.0;  //gets updated referencing lead car
 double min_vel = 15.0;   //gets updated referencing lead car
-double catchup_vel_offset = 5.0;
-double lead_vel; //retrieved from lead car speed parameter
-
-//max considered separation INVERTED
-double min_pid_src = -20;  //smaller than 5 and the vehicles collide
-double max_pid_src = 20;
-
+//PID reads this as input
+double pid_source = 0.0;  //a1_a2_separation - sep_target;
+//PID tries to get output to target
+double pid_target = 0.0;
 //PID constants
 double P = 8.5;
 double I = 0;
 double D = 0;
-double pid_source = 0.0;  //a1_a2_separation - sep_target;
-double pid_target = 0.0;
-//PID controller object
 
+//publishes velocity and steering command messages
 void cmdVel(double v)
 {
   geometry_msgs::Twist vel;
@@ -44,21 +37,13 @@ void cmdVel(double v)
   vel.angular.z = cmd_turn;
 
   pub_vel.publish(vel);
-  ROS_INFO("Published Velocity: %f", vel.linear.x);
-  ROS_INFO("Distance between cars: %f", a1_a2_separation);
+  //ROS_INFO("Published Velocity: %f", vel.linear.x);
+  //ROS_INFO("Distance between cars: %f", a1_a2_separation);
 }
 
-void recvLeadVel(const geometry_msgs::Twist& msg){
-  geometry_msgs::Twist follow_vel;
-  max_vel = follow_vel.linear.x + catchup_vel_offset;
-  min_vel = follow_vel.linear.x - catchup_vel_offset;
-  ROS_INFO("read lead vel");
-}
-
-//PID input
+//PID input/source/feedback
 double pidDoubleSource()
 {
-  ROS_INFO("PID double source: %f", pid_source);
   return pid_source;
 }
 //PID output
@@ -67,20 +52,11 @@ void pidDoubleOutput(double output)
   cmdVel(output);
 }
 
-unsigned long timeFunction()
-{
-  // Multiply by 1000 to convert seconds to milliseconds
-  return (unsigned long) ros::Time::now().toSec();
-}
-
 //initializes PID components
 void initPID(PIDController<double>& myDoublePIDControllerPtr){
-  //myDoublePIDControllerPtr.registerTimeFunction(timeFunction);
   myDoublePIDControllerPtr.setTarget(pid_target);
   myDoublePIDControllerPtr.setOutputBounded(true);
   myDoublePIDControllerPtr.setOutputBounds(min_vel, max_vel);
-  //myDoublePIDControllerPtr.setInputBounded(true);
-  //myDoublePIDControllerPtr.setInputBounds(min_pid_src, max_pid_src);
   myDoublePIDControllerPtr.setEnabled(true);
 }
 
@@ -102,7 +78,7 @@ void recvThr(const geometry_msgs::Twist& msg){
 //http://docs.ros.org/en/jade/api/gazebo_msgs/html/msg/ModelStates.html
 void recvModelStates(const gazebo_msgs::ModelStates& msg){
   int arraySize = msg.name.size();    //vehicle models are at end
-  //last is lead car?
+  //last in array is lead car?
   //second last is follow car?
   double a1x = msg.pose[arraySize-2].position.x;
   double a1y = msg.pose[arraySize-2].position.y;
@@ -122,13 +98,14 @@ void recvModelStates(const gazebo_msgs::ModelStates& msg){
 //init PID controller
 PIDController<double> vel_PID_controller(P, I, D, pidDoubleSource, pidDoubleOutput);
 
+//refreshes PID
 void PIDTimerCallback(const ros::TimerEvent& event){
   vel_PID_controller.tick();
-  ROS_INFO("PID ticked");
+  /*ROS_INFO("PID ticked");
   ROS_INFO("PID Target: %f", vel_PID_controller.getTarget());
   ROS_INFO("PID error: %f", vel_PID_controller.getError());
   ROS_INFO("PID output: %f", vel_PID_controller.getOutput());
-  ROS_INFO("PID feedback: %f", vel_PID_controller.getFeedback());
+  ROS_INFO("PID feedback: %f", vel_PID_controller.getFeedback()); */
 }
 
 //main function
@@ -137,17 +114,20 @@ int main(int argc, char** argv){
   ros::init(argc,argv,"follow_twist_override");
   ros::NodeHandle nh;
 
+  //initializing PID object, passes pointer
   PIDController<double> *ip = &vel_PID_controller;
   initPID(*ip);
 
-  nh.getParam("/a2/speed",lead_vel);
-
+  //subscriber for steering messages
   ros::Subscriber sub_cmd_vel = nh.subscribe("/a1/cmd_vel", 1, recvThr);
-  ros::Subscriber sub_lead_vel = nh.subscribe("a2/twist", 1, recvLeadVel);
 
+  //timer to refresh PID
   ros::Timer PID_timer = nh.createTimer(ros::Duration(0.01), PIDTimerCallback);
 
+  //for publishing steering and throttle messages
   pub_vel = nh.advertise<geometry_msgs::Twist>("/a1/cmd_vel", 1); //x velocity with float64
+
+  //getting vehicle positions
   ros::Subscriber sub_gazebo_spy = nh.subscribe("/gazebo/model_states", 1, recvModelStates);
 
   ros::spin();
